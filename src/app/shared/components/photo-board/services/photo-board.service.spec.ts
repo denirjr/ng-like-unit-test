206d6826e4efad89a0d957da8f0e065fa8491c29
Denir <denir-junior19@live.com>
2021-08-10 23:36:53 -0300

    feat(master): http client test module implementation

---
+++ src/app/shared/components/photo-board/services/photo-board.service.spec.ts
@@ -0,0 +1,46 @@
+import { TestBed } from '@angular/core/testing';
+import { PhotoBoardService } from './photo-board.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+
+const mockData = {
+  api: 'http://localhost:3000/photos',
+  data: [
+    {
+      id: 1,
+      description: 'batata 1',
+      src: '',
+    },
+    {
+      id: 2,
+      description: 'batata 2',
+      src: '',
+    },
+  ],
+};
+
+describe(PhotoBoardService.name, () => {
+  let service: PhotoBoardService;
+  let httpController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PhotoBoardService],
+    });
+
+    service = TestBed.inject(PhotoBoardService);
+    httpController = TestBed.inject(HttpTestingController);
+  });
+
+  it(`#${PhotoBoardService.prototype.getPhotos.name} should return photos with description in upper case`, (done) => {
+    service.getPhotos().subscribe((photos) => {
+      expect(photos[0].description).toBe('BATATA 1');
+      expect(photos[1].description).toBe('BATATA 2');
+      done();
+    });
+    httpController.expectOne(mockData.api).flush(mockData.data);
+  });
+});
--- src/app/shared/components/photo-board/services/photo-board.service.ts
+++ src/app/shared/components/photo-board/services/photo-board.service.ts
@@ -1,16 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { delay, map, tap } from 'rxjs/operators';
+import { PhotoFrameModule } from '../../photo-frame/photo-frame.module';
 import { Photo } from '../interfaces/photo';
+import { PhotoBoardModule } from '../photo-board.module';
 
 @Injectable()
 export class PhotoBoardService {
   constructor(private http: HttpClient) {}
 
   public getPhotos(): Observable<Photo[]> {
-    return this.http
-      .get<Photo[]>('http://localhost:3000/photos')
-      .pipe(delay(2000));
+    return this.http.get<Photo[]>('http://localhost:3000/photos').pipe(
+      map((photos) =>
+        photos.map((photo) => {
+          return { ...photo, description: photo?.description.toUpperCase() };
+        })
+      ),
+      tap(console.log),
+      delay(2000)
+    );
   }
 }
